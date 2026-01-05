43f37f0520ea9922a6608171145281315d398a3d
ROmina De León <romideleon@gmail.com>
2026-01-05 14:49:45 -0300

    add app.txt

--- install.R
+++ install.R
@@ -1,10 +1,18 @@
-install.packages(c(
-  "tidyverse", "RColorBrewer", "paletteer", "datos", "gapminder", "tradestatistics", "highcharter",
-  "treemapify", "ggplot2", "rio", "echarts4r", "plotly", "tidytext", "tidyselect", "dplyr", 
-  "purrr", "readr", "stringr", "webshot", "htmlwidgets", "pandoc", "forcats",
-"cowplot", "ggrepel", "sf", "rnaturalearth", "rnaturalearthhires", "countrycode", "scico", "Cairo",
-"colorspace", "Polychrome", "MetBrewer", "ggsci", "zoo", "ggiraph"  
-))
+packages <- c(
+  "tidyverse","RColorBrewer","paletteer","datos","gapminder",
+  "tradestatistics","highcharter","treemapify","ggplot2","rio",
+  "echarts4r","plotly","tidytext","tidyselect","dplyr","purrr",
+  "readr","stringr","webshot","htmlwidgets","pandoc","forcats",
+  "cowplot","ggrepel","sf","rnaturalearth","rnaturalearthhires",
+  "countrycode","scico","Cairo","colorspace","Polychrome",
+  "MetBrewer","ggsci","zoo","ggiraph"
+)
+
+install.packages(
+  packages,
+  dependencies = TRUE,
+  Ncpus = 2
+)
 
 # Registrar IRkernel para Jupyter
 IRkernel::installspec(user = FALSE)
