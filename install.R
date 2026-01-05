c184f87754a1a57b43af5809e95ef50a98e29eb1
ROmina De León <romideleon@gmail.com>
2026-01-05 15:09:08 -0300

    update install.r

--- install.R
+++ install.R
@@ -1,19 +1,17 @@
-packages <- c(
+install.packages(c(
+  "remotes",
   "tidyverse","RColorBrewer","paletteer","datos","gapminder",
   "tradestatistics","highcharter","treemapify","ggplot2","rio",
   "echarts4r","plotly","tidytext","tidyselect","dplyr","purrr",
   "readr","stringr","webshot","htmlwidgets","pandoc","forcats",
-  "cowplot","ggrepel","sf","rnaturalearth","rnaturalearthhires",
-  "countrycode","scico","Cairo","colorspace","Polychrome",
-  "MetBrewer","ggsci","zoo","ggiraph"
-)
+  "cowplot","ggrepel","sf","rnaturalearth","countrycode",
+  "scico","Cairo","colorspace","Polychrome","MetBrewer",
+  "ggsci","zoo","ggiraph"
+), dependencies = TRUE, Ncpus = 2)
 
-install.packages(
-  packages,
-  dependencies = TRUE,
-  Ncpus = 2
-)
+# paquete NO-CRAN
+if (!requireNamespace("rnaturalearthhires", quietly = TRUE)) {
+  remotes::install_github("ropensci/rnaturalearthhires")
+}
 
-# Registrar IRkernel para Jupyter
 IRkernel::installspec(user = FALSE)
-
